907e096b1005e2d212d305c7b4c7460a3414c85f
Mari8red <mariana8oneto@gmail.com>
2023-09-12 21:09:52 -0300

    entrega-2

--- js/login.js
+++ js/login.js
@@ -16,6 +16,9 @@ function login() {
     // Guardamos la información de la sesión en el almacenamiento local
     // Establecemos la clave "isLoggedIn" con el valor "true"
     localStorage.setItem("isLoggedIn", "true");
+
+    // Almacenamos el nombre de usuario
+    localStorage.setItem("username", username);   
     
     // Redireccionamos a la portada
     window.location.href = "index.html";
@@ -36,10 +39,5 @@ document.getElementById("password").addEventListener("keydown", function(event) 
         login(); // Llama a la función de inicio de sesión
     }
 });
-
-
-// Llama a la función actualizarBoton cada vez que los campos cambian
-document.getElementById("username").addEventListener("input", actualizarBoton);
-document.getElementById("password").addEventListener("input", actualizarBoton);
 
 
